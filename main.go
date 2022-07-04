0e9eae4afe36683d1060bede2fb00c369c2529e9
Aashish Paliwal <aashish.paliwal@live.com>
2022-07-04 18:04:26 +0530

    added functions definition - logic to be added later

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 //Using struct and slice for now, instead of database
 
 type Movie struct {
@@ -16,6 +24,37 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies() {
+
+}
+
+func getMovie() {
+
+}
+
+func createMovie() {
+
+}
+
+func updateMovie() {
+
+}
+
+func deleteMovie() {
+
+}
+
 func main() {
-	
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "12345", Title: "Movie 1", Director: &Director{Firstname: "Adam", Lastname: "Smith"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "12346", Title: "Movie 2", Director: &Director{Firstname: "Eve", Lastname: "Smith"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server at port 8000")
+	log.Fatal(http.ListenAndServe(":8000",r))
 }
